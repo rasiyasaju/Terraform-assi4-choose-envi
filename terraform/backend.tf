d80cc3664f2bc7096dc6764a7719be164c809e2a
Rasiya <rasiya25@gmail.com>
2026-05-28 14:13:04 +0100

    changed the backend bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-rasiya"
+    bucket         = "terraform-state-rasiya-2026"
     key            = "assignment4/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-lock"
