d3cf4c65808aa17ce401034c04903c2058e613ad
Rasiya <rasiya25@gmail.com>
2026-05-28 13:47:50 +0100

    added force_desroy =true

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "${var.environment}-rasiya-assi4-demo-bucket"
 
+    force_destroy = true
+
   tags = {
     Name        = "${var.environment}-rasiya-assi4-demo-bucket"
     Environment = var.environment
